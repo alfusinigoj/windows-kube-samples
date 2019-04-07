ee03dac02d84c6bf2c6e4d75fa357fcc4faabdb7
Chris Umbel <chris@chrisumbel.com>
2019-04-07 13:21:43 -0400

    removed batch file idea

--- isapi-module/setup-iis.cmd
+++
@@ -1,3 +0,0 @@
-c:/Windows/System32/inetsrv/appcmd.exe set config "Default Web Site" /section:system.webServer/httpErrors /errorMode:Detailed
-c:/Windows/System32/inetsrv/appcmd.exe set config -section:system.webServer/security/isapiCgiRestriction /+"[path='C:\Inetpub\wwwroot\isapi\PKSISAPI.dll',allowed='True',groupId='PivotalGroup',description='PKS Extension']" /commit:apphost
-c:/Windows/System32/inetsrv/appcmd.exe set config /section:handlers /+[name='PKSIsapiExtension',path='PKSISAPI.dll',verb='GET,POST',modules='IsapiModule',scriptProcessor='C:\Inetpub\wwwroot\isapi\PKSISAPI.dll'] /commit:apphost
