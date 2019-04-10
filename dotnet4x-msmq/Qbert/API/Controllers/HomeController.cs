6690eee221fdd57e906465fe4215d64aa4d436f0
chrisumbel <chris@chrisumbel.com>
2019-04-10 10:37:42 +0000

    removed junk, moved service address to web.config

--- dotnet4x-msmq/Qbert/API/Controllers/HomeController.cs
+++ dotnet4x-msmq/Qbert/API/Controllers/HomeController.cs
@@ -10,9 +10,7 @@ namespace API.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
-
-            return View();
+            return Content("Hello, World!");
         }
     }
 }
